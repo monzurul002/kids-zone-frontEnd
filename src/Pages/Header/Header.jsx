29c216e2eedda0b58c961b44e7534a44a039381d
monzurul002 <monzurul002@gmail.com>
2023-11-16 08:54:09 +0600

    header creating

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,4 +1,6 @@
 import bg from "../../assets/bg.jpg"
+import { AiOutlineShopping } from "react-icons/ai";
+
 const Header = () => {
     const commonBackgroundStyle = {
         backgroundImage: `url(${bg})`,
@@ -15,6 +17,15 @@ const Header = () => {
                 <p className="py-2 text-xl text-slate-300">How old is the first toy?
                     4,000 years old
                     The origin of the word toy is unknown, but it is believed that it was first used in the 14th century. Toys are mainly made for children. The oldest known doll toy is thought to be 4,000 years old.</p>
+
+                <div className="flex w-full text-white gap-3 py-5">
+                    <AiOutlineShopping className="text-8xl w-50 me-3 text-white" />
+                    <div>
+                        <h2 className="text-2xl font-bold ">SINGLE PRODUCT SHOWCASES
+                        </h2>   <p> The earliest toys were made from rocks, sticks or clay. In Ancient Egypt, children played with dolls made of clay and sticks.</p>
+                    </div>
+
+                </div>
             </div>
             <div>
 
